f5eb90549904f1f047d479d163236569de20ba8f
Kekanium <stas280501@yandex.ru>
2021-06-23 12:07:25 +0300

    Add prep Cell and algorithms

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,14 @@
+public class Cell
+{
+   private bool Up
+   {
+      get;
+      set;
+   }
+   private bool Left
+   {
+      get;
+      set;
+   }
+   
+}
---
+++ Assets/Scripts/GenerateAlgorithms.cs
@@ -0,0 +1,22 @@
+public class GenerateAlgorithms
+{
+    public Cell[,] NameAlgorith(int rows, int colums)
+    {
+        Cell[,] layout = new Cell[rows,colums];
+        
+        
+        
+        
+        return layout;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
