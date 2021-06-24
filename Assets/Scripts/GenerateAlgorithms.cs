21a80f61ec82c85b76803c80a1a32c82488e6e27
Kekanium <stas280501@yandex.ru>
2021-06-25 02:30:53 +0300

    Working version 25.06.2021

--- Assets/Scripts/GenerateAlgorithms.cs
+++ Assets/Scripts/GenerateAlgorithms.cs
@@ -1,18 +1,20 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
 
 public static class GenerateAlgorithms
 {
     public static Cell[,] AlgorithmBinaryTrees(int rows, int columns)
     {
-        Cell[,] maze = new Cell[rows,columns];
-        
-        var rand = new System.Random();
+        var maze = new Cell[rows, columns];
 
-        for (int i = rows - 1; i >= 0; --i)
-        for (int j = columns - 1; j >= 0; --j)
+        var rand = new Random();
+
+        for (var i = rows - 1; i >= 0; --i)
+        for (var j = columns - 1; j >= 0; --j)
         {
             maze[i, j] = new Cell();
-            if (rand.Next() % 2 == 0)
+            if (rand.Next(0, 1) == 0)
             {
                 if (j == 0)
                     maze[i, j].Up = false;
@@ -21,26 +23,13 @@ public static class GenerateAlgorithms
             }
             else
             {
-                if (i == (rows-1))
+                if (i == (rows - 1))
                     maze[i, j].Left = false;
                 else
                     maze[i, j].Up = false;
             }
         }
-        
-        
-        
-        
+
         return maze;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
